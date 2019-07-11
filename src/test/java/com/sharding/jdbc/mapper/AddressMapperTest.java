9bb9b2b221940e76ad716a123bfd19ed704f8da1
qiuwei <Wcbc0310@126.com>
2019-07-11 15:54:20 +0800

    添加两个单元测试

---
+++ src/test/java/com/sharding/jdbc/mapper/AddressMapperTest.java
@@ -0,0 +1,17 @@
+package com.sharding.jdbc.mapper;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author: qiuwei@19pay.com.cn
+ * @Version 1.0.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AddressMapperTest {
+
+}
