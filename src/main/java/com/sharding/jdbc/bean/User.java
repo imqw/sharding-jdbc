3fd0adf117d609b18aee7c1dd5c75033d59eb0d3
qiuwei <Wcbc0310@126.com>
2019-07-11 15:37:00 +0800

    添加两实例

---
+++ src/main/java/com/sharding/jdbc/bean/User.java
@@ -0,0 +1,101 @@
+package com.sharding.jdbc.bean;
+
+import java.util.Date;
+
+/**
+ * @Author: qiuwei@19pay.com.cn
+ * @Version 1.0.0
+ */
+public class User {
+
+    private Long id;
+    private String name;
+    private String phone;
+    private String email;
+    private String password;
+    private Integer cityId;
+    private Date createTime;
+    private Integer sex;
+
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", cityId=" + cityId +
+                ", createTime=" + createTime +
+                ", sex=" + sex +
+                '}';
+    }
+}
